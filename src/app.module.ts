8fdf7391a69f076f25c12c6e36475b1ffa2b6ce1
Hector Ramirez <hector.ramirez.her20@gmail.com>
2026-05-20 21:16:59 -0600

    Added Products CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 
+import { ProductsModule } from './products/products.module';
+
 @Module({
-  imports: [EmployeesModule],
+  imports: [EmployeesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
